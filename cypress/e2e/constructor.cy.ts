eabeb139f17a44b6a01b86f6e59e9fd0dc665d1e
Anton <dubovik02@yandex.ru>
2025-11-19 14:24:06 +0300

    Tests have done + Readme - Fixed

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,39 +1,50 @@
+import {
+  TEST_BASE_URL,
+  TEST_BUN_SELECTOR,
+  TEST_CONSTRUCTOR_SELECTOR,
+  TEST_INGR_DETAIL_HEADER,
+  TEST_MAIN_SELECTOR,
+  TEST_MODAL_SELECTOR,
+  TEST_OVERLAY_SELECTOR,
+  TEST_SAUCE_SELECTOR,
+} from './testPropperties';
+
 describe('Constructor test', () => {
   beforeEach((): void => {
-    cy.visit('http://localhost:5173');
+    cy.visit(TEST_BASE_URL);
   });
 
   it('should show ingredient detail after click on element', () => {
     //close by click
-    cy.get('[data-card-test="bun"]').first().click();
-    cy.get('[data-card-test="modal"]').should('contain.text', 'Детали ингридиента');
+    cy.get(`${TEST_BUN_SELECTOR}`).first().click();
+    cy.get(`${TEST_MODAL_SELECTOR}`).should('contain.text', TEST_INGR_DETAIL_HEADER);
     cy.wait(2000);
-    cy.get('[data-card-test="overlay"]').click('topLeft');
+    cy.get(`${TEST_OVERLAY_SELECTOR}`).click('topLeft');
 
     //close by esc
     cy.wait(2000);
-    cy.get('[data-card-test="main"]').first().click();
-    cy.get('[data-card-test="modal"]').should('contain.text', 'Детали ингридиента');
+    cy.get(`${TEST_MAIN_SELECTOR}`).first().click();
+    cy.get(`${TEST_MODAL_SELECTOR}`).should('contain.text', TEST_INGR_DETAIL_HEADER);
     cy.wait(2000);
     cy.get('body').type('{esc}');
 
     //close by click on close icon
     cy.wait(2000);
-    cy.get('[data-card-test="sauce"]').first().click();
-    cy.get('[data-card-test="modal"]').should('contain.text', 'Детали ингридиента');
+    cy.get(`${TEST_SAUCE_SELECTOR}`).first().click();
+    cy.get(`${TEST_MODAL_SELECTOR}`).should('contain.text', TEST_INGR_DETAIL_HEADER);
     cy.wait(2000);
     cy.get('*[class^="modal__modal__close"]').click();
   });
 
   it('should drag and drop ingredients to the constructor', () => {
     cy.wait(2000);
-    cy.get('[data-card-test="bun"]').eq(0).trigger('dragstart');
-    cy.get('[data-card-test="constructor"]').trigger('drop');
+    cy.get(`${TEST_BUN_SELECTOR}`).eq(0).trigger('dragstart');
+    cy.get(`${TEST_CONSTRUCTOR_SELECTOR}`).trigger('drop');
     cy.wait(2000);
-    cy.get('[data-card-test="sauce"]').eq(1).trigger('dragstart');
-    cy.get('[data-card-test="constructor"]').trigger('drop');
+    cy.get(`${TEST_SAUCE_SELECTOR}`).eq(1).trigger('dragstart');
+    cy.get(`${TEST_CONSTRUCTOR_SELECTOR}`).trigger('drop');
     cy.wait(2000);
-    cy.get('[data-card-test="main"]').eq(2).trigger('dragstart');
-    cy.get('[data-card-test="constructor"]').trigger('drop');
+    cy.get(`${TEST_MAIN_SELECTOR}`).eq(2).trigger('dragstart');
+    cy.get(`${TEST_CONSTRUCTOR_SELECTOR}`).trigger('drop');
   });
 });
---
+++ cypress/e2e/testPropperties.ts
@@ -0,0 +1,12 @@
+export const TEST_BASE_URL = 'http://localhost:5173';
+
+export const TEST_BUN_SELECTOR = '[data-card-test="bun"]';
+export const TEST_MAIN_SELECTOR = '[data-card-test="main"]';
+export const TEST_SAUCE_SELECTOR = '[data-card-test="sauce"]';
+
+export const TEST_MODAL_SELECTOR = '[data-card-test="modal"]';
+export const TEST_OVERLAY_SELECTOR = '[data-card-test="overlay"]';
+
+export const TEST_CONSTRUCTOR_SELECTOR = '[data-card-test="constructor"]';
+
+export const TEST_INGR_DETAIL_HEADER = 'Детали ингридиента';
