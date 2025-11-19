6a6942788fca17174ad4b764b78563210dd07cb8
Anton <dubovik02@yandex.ru>
2025-11-19 08:36:56 +0300

    Tests have done

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,9 +1,9 @@
 describe('Constructor test', () => {
-  before((): void => {
+  beforeEach((): void => {
     cy.visit('http://localhost:5173');
   });
 
-  it('should show ingredient detail after click on 1st element', () => {
+  it('should show ingredient detail after click on element', () => {
     //close by click
     cy.get('[data-card-test="bun"]').first().click();
     cy.get('[data-card-test="modal"]').should('contain.text', 'Детали ингридиента');
@@ -23,8 +23,10 @@ describe('Constructor test', () => {
     cy.get('[data-card-test="modal"]').should('contain.text', 'Детали ингридиента');
     cy.wait(2000);
     cy.get('*[class^="modal__modal__close"]').click();
+  });
 
-    //drag'n'drop ingredients
+  it('should drag and drop ingredients to the constructor', () => {
+    cy.wait(2000);
     cy.get('[data-card-test="bun"]').eq(0).trigger('dragstart');
     cy.get('[data-card-test="constructor"]').trigger('drop');
     cy.wait(2000);
@@ -34,27 +36,4 @@ describe('Constructor test', () => {
     cy.get('[data-card-test="main"]').eq(2).trigger('dragstart');
     cy.get('[data-card-test="constructor"]').trigger('drop');
   });
-
-  // it('should drag and drop ingredients to the constructor', () => {
-  //   cy.get('[data-card-test="bun"]').eq(0).trigger('dragstart');
-  //   cy.get('[data-card-test="constructor"]').trigger('drop');
-
-  //   cy.get('[data-card-test="sauce"]').trigger('dragstart');
-  //   cy.get('[data-card-test="constructor"]').trigger('drop');
-
-  //   cy.get('[data-card-test="main"]').trigger('dragstart');
-  //   cy.get('[data-card-test="constructor"]').trigger('drop');
-  // });
-
-  // it('should create order', () => {
-  //   cy.contains('заказ').click();
-  //   cy.wait(20000);
-  //   cy.get('[data-card-test="modal"]').should('contain.text', 'заказ');
-  //   cy.wait(3000);
-  //   cy.get('[data-card-test="overlay"]').click('topLeft');
-  // });
-
-  // it('should constructor has cleared', () => {
-  //   cy.get('[data-card-test="constructor"]').children().should('have.length', 2);
-  // });
 });
